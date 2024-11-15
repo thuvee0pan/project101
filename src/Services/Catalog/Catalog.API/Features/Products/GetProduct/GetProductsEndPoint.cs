02d1f50fc8eb42cd6748df6ceb79c2d196fb8911
thuvee <tthuvee@gmail.com>
2024-11-15 22:57:18 +0530

    GetProductsEndPoint

---
+++ src/Services/Catalog/Catalog.API/Features/Products/GetProduct/GetProductsEndPoint.cs
@@ -0,0 +1,23 @@
+namespace Catalog.API.Features.Products.GetProduct;
+
+public record GetProductResponse(IEnumerable<Product> Products);
+
+public class GetProductsEndPoint: ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/products", async (ISender sender) =>
+        {
+            var result = await sender.Send(new GetProductQuery());
+
+            var response = result.Adapt<GetProductResponse>();
+            
+            return response;
+        })
+            .WithName("GetProducts")
+            .Produces(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get products")
+            .WithDescription("Get products");
+    }
+}
